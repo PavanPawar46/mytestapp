4578e80c83aae0ebbdd3e097519a5b9664dd2237
Pavan Pawar <pawarpavan57@gmail.com>
2020-12-10 17:40:47 +0530

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcomew to jenkins");
+        System.out.println("hello");
+    
+    }
+}
